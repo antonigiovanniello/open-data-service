9e6d622a2a8fd9d8a50d6a88cb682fa571885b1e
nxmyoz <v-github@9dt.de>
2020-07-27 15:50:34 +0200

    Small fixes in kube.config.

--- deployment/mk-kubeconfig.sh
+++ deployment/mk-kubeconfig.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 cat > kube.config <<EOF
 apiVersion: v1
-kind: Config
-users:
-- name: ${K8S_SERVICEACCOUNT_NAME}
-  user:
-    token: ${K8S_SERVICEACCOUNT_TOKEN}
 clusters:
 - cluster:
     certificate-authority-data: ${K8S_CLUSTER_CERTIFICATE_AUTHORITY_DATA}
@@ -17,4 +12,10 @@ contexts:
     user: ${K8S_SERVICEACCOUNT_NAME}
   name: ods-deployment-context
 current-context: ods-deployment-context
+kind: Config
+preferences: {}
+users:
+- name: ${K8S_SERVICEACCOUNT_NAME}
+  user:
+    token: ${K8S_SERVICEACCOUNT_TOKEN}
 EOF
